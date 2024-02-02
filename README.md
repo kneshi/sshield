5216666e8c5c873a9419c6d1164d736265eda8de
kneshi <gaetan.randria@hotmail.com>
2024-02-02 09:42:58 +0100

    feat: new features + refacto

--- README.md
+++ README.md
@@ -1,34 +1,30 @@
-# SSH Server Configuration Generator
-
+# SSHield
 ⚠️ WARNING: Configuration contains random data for testing purposes only. SSH configuration depends on various factors including the specific use case, network environment, and organizational policies, so, use this script with care. ⚠️
---------
 
-This script generate an SSH server configuration based on recommendations from either ANSSI (French Security Agency) or CIS (Center for Internet Security).
+SSHield is a script that helps you secure your SSH server by generating a configuration based on your preferences.
 
-## Running the Script
+## Sources
 
-1. `chmod +x sshield.sh`
-2. `./sshield.sh`
-
-## Parameter Sets
+The recommendations for the parameter values are based on the following sources:
 
 - ANSSI (2015): [French Security Agency Recommendations](https://www.ssi.gouv.fr/guide/recommandations-pour-un-usage-securise-dopenssh/)
 - CIS Debian 11: [Center for Internet Security Recommendations - Debian 11](https://downloads.cisecurity.org/#/)
 
 ## How It Works
 
-This script reads parameter information from `configs/` directory.
-Each parameter consists of a name, comment, and recommended value.
-You will be prompted to enter your preferred value for each parameter.
+SSHield reads parameter information from the `configs/` directory. Each parameter consists of a name, comment, and recommended value. You will be prompted to enter your preferred value for each parameter.
 
-The generated configuration is saved to `sshd_config_generated.txt`.
-Optionally, you can choose to overwrite `/etc/ssh/sshd_config` and restart the SSH server.
+The generated configuration is saved to `sshd_config_generated.txt`. Optionally, you can choose to overwrite `/etc/ssh/sshd_config` and restart the SSH server.
+
+## Usage
+
+1. Clone the repository: `git clone https://github.com/kneshi/sshield.git`
+2. Navigate to the cloned directory: `cd sshield`
+3. Run the script: `./sshield.sh`
 
 ## Todo
 
-- [ ] populate configs files
-- [x] add descriptions as comment inside the sshd_config
-- [x] add an options to skip and go to next 
-- [ ] test /usr/sbin/sshd -t -f
-- [ ] set mode and ownership on the sshd_config file if pushed to prod (root:root and 0600)
-- [ ] add a mandatory options to be sure we don't forget a parameters
+- [ ] Populate configs files
+- [ ] Test /usr/sbin/sshd -t -f
+- [ ] Set mode and ownership on the sshd_config file if pushed to prod (root:root and 0600)
+- [ ] Add a mandatory option to be sure we don't forget a parameter
--- sshield.sh
+++ sshield.sh
@@ -3,10 +3,48 @@
 # Define color variables
 bold_blue="\e[1;34m"
 bold_green="\e[1;32m"
+bold_red="\e[1;31m"
 reset="\e[0m"
 
-# Select params file
-choose_params_file() {
+# Function to check if sudo command is available and if the user has sudo privileges
+check_sudo() {
+    if ! command -v sudo &> /dev/null; then
+        echo -e "${bold_red}Error: sudo command not found.${reset}"
+        exit 1
+    fi
+
+    if ! sudo -n true 2>/dev/null; then
+        echo -e "${bold_red}Error: You do not have sudo privileges or a password is required (try with: sudo ./sshield.sh).${reset}"
+        exit 1
+    fi
+}
+
+
+# Function to check if sshd command is available and install OpenSSH server if not found
+check_sshd() {
+    if ! command -v sshd &> /dev/null; then
+        echo -e "${bold_red}Error: sshd command not found.${reset}"
+        read -p "Do you want to install OpenSSH server? (y/n) > " -r choice
+        if [[ $choice =~ ^[Yy]$ ]]; then
+            if command -v apt-get &> /dev/null; then
+                sudo apt-get install openssh-server || { echo -e "${bold_red}Error: Failed to install OpenSSH server.${reset}"; exit 1; }
+            elif command -v yum &> /dev/null; then
+                sudo yum install openssh-server || { echo -e "${bold_red}Error: Failed to install OpenSSH server.${reset}"; exit 1; }
+            elif command -v dnf &> /dev/null; then
+                sudo dnf install openssh-server || { echo -e "${bold_red}Error: Failed to install OpenSSH server.${reset}"; exit 1; }
+            else
+                echo -e "${bold_red}Error: Unsupported package manager.${reset}"
+                exit 1
+            fi
+        else
+            echo -e "${bold_red}Error: sshd command is required.${reset}"
+            exit 1
+        fi
+    fi
+}
+
+# Function to select a params file
+select_params_file() {
     local options=()
     local choice
 
@@ -24,7 +62,7 @@ choose_params_file() {
     echo -e ""
 
     if [[ ! $choice =~ ^[0-9]+$ ]] || ((choice < 0 || choice >= ${#options[@]})); then
-        echo "Invalid choice. Please retry with a valid option."
+        echo -e "${bold_red}Invalid choice. Please retry with a valid option.${reset}"
         exit 1
     fi
 
@@ -72,24 +110,27 @@ create_field() {
     echo "$value"
 }
 
+# Function to generate the SSH server configuration
 generate_config() {
     local config=""
     config+="Include /etc/ssh/sshd_config.d/*.conf\n\n"
     for param_info in "${params[@]}"; do
         local value=$(create_field "$param_info")
         if [ "$value" != "!" ]; then
-            config+="$(echo "# $param_info" | cut -d ':' -f 2)"
-            config+="$(echo "$param_info" | cut -d ':' -f 1) $value\n\n"
+            config+="# ${param_info#*:}\n"
+            config+="${param_info%%:*} $value\n\n"
         fi
         echo >&2
     done
     echo -e "$config" >sshd_config_generated.md
+}
 
-    # Check the generated config - TODO : check this function
-    # if ! /usr/sbin/sshd -t -f sshd_config_generated.txt; then
-    #     echo "Error: There is a problem with the generated configuration. Please check sshd_config_generated.txt."
-    #     exit 1
-    # fi
+# Function to check the generated configuration for errors
+check_generated_config() {
+        if ! sshd -t -f sshd_config_generated.md; then
+            echo -e "${bold_red}Error: There is a problem with the generated configuration. Please check sshd_config_generated.md${reset}"
+            exit 1
+        fi
 }
 
 # Function to restart SSH based on the Linux distribution
@@ -98,41 +139,65 @@ restart_ssh() {
         source /etc/os-release
         case "$ID" in
         debian | ubuntu)
-            sudo service ssh restart || echo "Failed to restart SSH. You may need to do it manually."
+            if ! sudo service ssh restart; then
+                echo -e "${bold_red}Failed to restart SSH. You may need to do it manually.${reset}"
+            fi
             ;;
         centos | rhel | fedora)
-            sudo systemctl restart sshd || echo "Failed to restart SSH. You may need to do it manually."
+            if ! sudo systemctl restart sshd; then
+                echo -e "${bold_red}Failed to restart SSH. You may need to do it manually.${reset}"
+            fi
+            ;;
+        alpine)
+            if ! sudo rc-service sshd restart; then
+                echo -e "${bold_red}Failed to restart SSH. You may need to do it manually.${reset}"
+            fi
             ;;
         *)
-            echo "Unsupported Linux distribution. You may need to manually restart SSH."
+            echo -e "${bold_red}Unsupported Linux distribution. You may need to manually restart SSH.${reset}"
             ;;
         esac
     else
-        echo "Unable to determine the Linux distribution. You may need to manually restart SSH."
+        echo -e "${bold_red}Unable to determine the Linux distribution. You may need to manually restart SSH.${reset}"
     fi
 }
 
 # Main function
 main() {
     echo -e "${bold_blue}SSH Server Configuration Generator${reset}"
-    echo -e "This script will help you generate an SSH server configuration."
+    echo -e "This script will guide you through the process of generating a secure SSH server configuration."
+    echo -e "Please follow the prompts and provide the necessary information."
     echo -e ""
-    echo -e "${bold_blue}How It Works :${reset}"
+    check_sudo
+    check_sshd
+    echo -e ""
+    echo -e "${bold_blue}How This Script Works :${reset}"
     echo -e "- Press 'Enter' for each parameter to use the recommended value."
     echo -e "- Press '!' to skip a parameter (it will not be added to the configuration)."
     echo -e ""
-    choose_params_file
+    select_params_file
     read_params
     local config=$(generate_config)
     echo -e "${bold_green}$config${reset}"
     echo -e "Configuration saved to ${bold_blue}sshd_config_generated.txt${reset}"
 
+    check_generated_config
+
     read -p "Do you want to overwrite /etc/ssh/sshd_config and restart SSH? (y/n) > " -r choice
     if [[ $choice =~ ^[Yy]$ ]]; then
-        sudo cp sshd_config_generated.txt /etc/ssh/sshd_config
+        sudo cp sshd_config_generated.md /etc/ssh/sshd_config
         restart_ssh
         echo -e "SSH configuration updated and SSH server restarted."
     fi
+
+    read -p "Do you want to set ownership (root:root) and permissions (0600) on /etc/ssh/sshd_config? (y/n) > " -r set_permissions
+    if [[ $set_permissions =~ ^[Yy]$ ]]; then
+        sudo chown root:root /etc/ssh/sshd_config
+        sudo chmod 0600 /etc/ssh/sshd_config
+        echo -e "Ownership and permissions set on /etc/ssh/sshd_config."
+    fi
+
+
     echo -e ""
     echo -e "${bold_blue}Great! Enjoy a more secure SSH server!${reset}"
     echo -e ""
